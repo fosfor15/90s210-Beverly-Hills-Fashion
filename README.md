2971ee97fe3c2abe6a631b20068856d9e94bf367
Dmitry Kostyuk <dima.kostyuk1508@gmail.com>
2022-12-12 17:17:11 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 90s210 Beverly Hills Fashion
+
+This is eCommerce React project dedicated to the brightest 90s fashion inspired by one of the coolest TV show [Beverly Hills 90210](https://en.wikipedia.org/wiki/Beverly_Hills,_90210)
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header>
+                <h1>
+                    90<sup>s</sup>210&#8199;
+                    <span>Beverly Hills Fashion</span>
+                </h1>
+            </header>
+        </div>
+    );
+}
+
+export default App;
